ad3528fa28cd0c639a5e1da7723dbd408772fe92
Kalicz Péter <kaliczp@gmail.com>
2025-02-25 12:39:08 +0100

    Daily precipitation

--- PrecProcess.R
+++ PrecProcess.R
@@ -6,3 +6,9 @@ plot(GHalomraw[,"r"], type = "h")
 GHtime <- as.Date(paste(substr((GHalomraw[,"Time"]),1,4), substr((GHalomraw[,"Time"]),5,6), substr((GHalomraw[,"Time"]),7,8), sep = "-"))
 diff(GHtime)
 GHtime[7059:7061] ## Hiányzik 2021-04-30 és 2022-01-01 között
+
+library(xts)
+GHalom <- xts(GHalomraw[,"r"], GHtime)
+plot(GHalom, type = "h")
+
+write.zoo(GHalom, "GHalom.csv", sep = ";", dec = ",")
