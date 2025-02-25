a0ca148f5f9578062af44e01a9b93561443b091a
Kalicz Péter <kaliczp@gmail.com>
2025-02-25 12:28:21 +0100

    Date test

--- PrecProcess.R
+++ PrecProcess.R
@@ -2,3 +2,7 @@ GHalomraw <- read.table("HABP_1RD_20020101_20071231_13601.csv", sep = ";", dec =
 GHalomraw <- rbind(GHalomraw,read.table("HABP_1RD_20080101_20231231_13600.csv", sep = ";", dec = ".", head = TRUE))
 
 plot(GHalomraw[,"r"], type = "h")
+
+GHtime <- as.Date(paste(substr((GHalomraw[,"Time"]),1,4), substr((GHalomraw[,"Time"]),5,6), substr((GHalomraw[,"Time"]),7,8), sep = "-"))
+diff(GHtime)
+GHtime[7059:7061] ## Hiányzik 2021-04-30 és 2022-01-01 között
